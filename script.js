20970b3f689f11dedc31d175f8d2c48f0bdd29f7
fernando <fernando@shopee.com>
2023-11-10 22:20:25 +0700

    feat: add javascript

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-filterSelection("all"); // Execute the function and show all columns
+filterSelection("all");
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -22,7 +20,6 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -35,7 +32,6 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
@@ -44,4 +40,59 @@ for (var i = 0; i < btns.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  if (!form.checkValidity()) {
+    event.preventDefault();
+  } else {
+  }
+});
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const storedEmail = localStorage.getItem("email");
+  const storedPassword = localStorage.getItem("password");
+
+  if (email === storedEmail && password === storedPassword) {
+    alert("Login successful");
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid email or password. Please try again.");
+  }
+}
+
+function saveToLocalStorage() {
+  const fullNameInput = document.getElementById("full-name");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  const fullName = fullNameInput.value.trim();
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (fullName.length < 5) {
+    alert("Full Name must be at least 5 characters.");
+    fullNameInput.focus();
+  } else if (!isValidEmail(email)) {
+    alert("Invalid email format. Please enter a valid email.");
+    emailInput.focus();
+  } else if (password.length < 5) {
+    alert("Password must be at least 5 characters.");
+    passwordInput.focus();
+  } else {
+    localStorage.setItem("fullName", fullName);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    alert("Account created successfully!");
+    window.location.href = "index.html";
+  }
+}
+
+function isValidEmail(email) {
+  return email.includes("@");
 }
